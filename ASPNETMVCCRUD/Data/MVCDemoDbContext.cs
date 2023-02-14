b37acd16ec087b0b333c57d5e67b39968bb2cd8d
gitbaloo <102722373+gitbaloo@users.noreply.github.com>
2023-02-14 10:15:39 +0100

    Add project files.

---
+++ ASPNETMVCCRUD/Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETMVCCRUD.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        public DbSet<Models.Domain.Employee> Employees { get; set; }
+    }
+}
